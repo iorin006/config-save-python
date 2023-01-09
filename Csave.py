32c41e96a1413a42b68327df314205f55789bae3
iorin006 <108205818+iorin006@users.noreply.github.com>
2023-01-09 06:18:52 +0000

    Add files via upload

--- Csave.py
+++ Csave.py
@@ -1,9 +1,20 @@
 import shutil
-print('・デスクトップにconfigができていればコピー成功です。\n・エンターを押した瞬間消えるとエラーが出てます。')
-name=input('PCのユーザーネームを入力してください::')
+print(' ██████  ██████  ███    ██ ███████ ██  ██████      ███████  █████  ██    ██ ███████ ')
+print('██      ██    ██ ████   ██ ██      ██ ██           ██      ██   ██ ██    ██ ██    ')  
+print('██      ██    ██ ██ ██  ██ █████   ██ ██   ███     ███████ ███████ ██    ██ █████   ')
+print('██      ██    ██ ██  ██ ██ ██      ██ ██    ██          ██ ██   ██  ██  ██  ██    ')  
+print(' ██████  ██████  ██   ████ ██      ██  ██████      ███████ ██   ██   ████   ███████ ') 
+print("")
+print('・デスクトップにconfigができていればコピー成功です。\n・エンターを押した瞬間消えるとエラーが出てます。\n・ファイルの上書きはできません')
+print('')
+print('PCのユーザーネームを入力してください')
+name=input(">")
+print(f"user name:{name}")
+print('')
 def c():
     print('コピーする方法を選んでください。\n1・configファイルだけをコピー\n2・マインクラフトのファイルごとコピー')
-    i=int(input())
+    print('')
+    i=int(input(">"))
     if i==1:
         print('コピー中です。操作しないでください。')
         shutil.copytree(f'C:\\Users\\{name}\\AppData\\Local\\Packages\\Microsoft.MinecraftUWP_8wekyb3d8bbwe\\RoamingState',f'C:\\Users\\{name}\\Desktop\\config')
